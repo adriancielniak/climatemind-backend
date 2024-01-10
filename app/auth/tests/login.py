ef15ec169e751837e7cecb0c2ecb4d518a7cf2d5
Tomasz Pochroń <tomasz.pochron29@gmail.com>
2024-01-10 20:02:50 +0100

    login test

---
+++ app/auth/tests/login.py
@@ -0,0 +1,51 @@
+import unittest
+import requests
+
+correctemail = %input
+correctpassword = %input
+
+class TestLogin(unittest.TestCase):
+    BASE_URL = "http://localhost:5000/login"
+    HEADERS = {'Accept': 'application/json', 'Authorization': 'Bearer 123', 'Content-Type': 'application/json'}
+
+    def test_successful_login(self):
+        payload = {
+            "email": correctemail,
+            "password": correctpassword
+        }
+        response = requests.post(self.BASE_URL, headers=self.HEADERS, json=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("access_token", response.json())
+
+    def test_incorrect_credentials(self):
+        payload = {
+            "email": "wrong@example.com",
+            "password": "wrongPassword"
+        }
+        response = requests.post(self.BASE_URL, headers=self.HEADERS, json=payload)
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_missing_credentials(self):
+        payload = {
+            "email": "testds1c1@example.com"
+        }
+        response = requests.post(self.BASE_URL, headers=self.HEADERS, json=payload)
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_missing_credentials1(self):
+        payload = {
+            "email": correctemail
+        }
+        response = requests.post(self.BASE_URL, headers=self.HEADERS, json=payload)
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_missing_credentials2(self):
+        payload = {
+            "password": correctpassword
+        }
+        response = requests.post(self.BASE_URL, headers=self.HEADERS, json=payload)
+        self.assertNotEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
