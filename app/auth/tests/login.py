b19064eb32cb83a3b77bd1960607c28ec4cac6bf
Tomasz Pochroń <tomasz.pochron29@gmail.com>
2024-01-11 22:16:33 +0100

    Update login.py

--- app/auth/tests/login.py
+++ app/auth/tests/login.py
@@ -1,48 +1,85 @@
-import unittest
-import requests
-###########################################################################################
-######           Remember to input data below          ####################################
-###########################################################################################
+import pytest
+from flask import url_for
+from mock import mock
 
-correctemail = #input
-correctpassword = #input
+from app.common.tests.utils import assert_email_sent
+from app.factories import faker, ScoresFactory
 
-class TestLogin(unittest.TestCase):
-    BASE_URL = "http://localhost:5000/login"
-    HEADERS = {'Accept': 'application/json', 'Authorization': 'Bearer 123', 'Content-Type': 'application/json'}
+@pytest.mark.integration
+def test_failed_login_nonexistent_user(client):
+    response = client.post(
+        url_for("auth.login"),
+        json={
+            "email": "nonexistent@example.com",
+            "password": "password123"
+        },
+    )
+    assert response.status_code == 401
+    assert response.json == {"error": "User does not exist."}
 
-    def test_successful_login(self):
-        payload = {
-            "email": correctemail,
-            "password": correctpassword
-        }
-        response = requests.post(self.BASE_URL, headers=self.HEADERS, json=payload)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("access_token", response.json())
+@pytest.mark.integration
+def test_failed_login_wrong_password(client):
+    registration = get_fake_registration_json()
+    client.post(url_for("auth.register"), json=registration)
+    response = client.post(
+        url_for("auth.login"),
+        json={
+            "email": registration["email"],
+            "password": "wrongpassword"
+        },
+    )
+    assert response.status_code == 401
+    assert response.json == {"error": "Wrong password."}
 
-    def test_incorrect_credentials(self):
-        payload = {
-            "email": "wrong@example.com",
-            "password": "wrongPassword"
-        }
-        response = requests.post(self.BASE_URL, headers=self.HEADERS, json=payload)
-        self.assertNotEqual(response.status_code, 200)
+@pytest.mark.integration
+def test_failed_login_without_email(client):
+    registration_data = get_fake_registration_json()
+    client.post(url_for("auth.register"), json=registration_data)
+    response = client.post(
+        url_for("auth.login"),
+        json={"password": registration_data["password"]}
+    )
+    assert response.status_code == 400
+    assert response.json == {"error": "Email and password must be included in the request body."}
+
+@pytest.mark.integration
+def test_failed_login_without_password(client):
+    registration_data = get_fake_registration_json()
+    client.post(url_for("auth.register"), json=registration_data)
+    response = client.post(
+        url_for("auth.login"),
+        json={"email": registration_data["email"]}
+    )
+    assert response.status_code == 400
+    assert response.json == {"error": "Email and password must be included in the request body."}
+
+@pytest.mark.integration
+def test_failed_login_with_nonexistent_email(client):
+    response = client.post(
+        url_for("auth.login"),
+        json={
+            "email": "nonexistent@example.com",
+            "password": "password123"
+        },
+    )
+    assert response.status_code == 401
+    assert response.json == {"error": "Wrong email or password. Try again."}
+
+@pytest.mark.integration
+@mock.patch("app.auth.routes.check_if_local")
+def test_failed_login_with_invalid_recaptcha_token(m_check_if_local, client):
+    m_check_if_local.return_value = False  # to enforce recaptchaToken validation
+    registration = get_fake_registration_json()
+    client.post(url_for("auth.register"), json=registration)
+    response = client.post(
+        url_for("auth.login"),
+        json={
+            "email": registration["email"],
+            "password": registration["password"],
+            "recaptchaToken": "invalidToken"
+        },
+    )
+    assert response.status_code == 401
+    assert "error" in response.json
 
 
-    def test_missing_credentials1(self):
-        payload = {
-            "email": correctemail
-        }
-        response = requests.post(self.BASE_URL, headers=self.HEADERS, json=payload)
-        self.assertNotEqual(response.status_code, 200)
-
-    def test_missing_credentials2(self):
-        payload = {
-            "password": correctpassword
-        }
-        response = requests.post(self.BASE_URL, headers=self.HEADERS, json=payload)
-        self.assertNotEqual(response.status_code, 200)
-
-
-if __name__ == '__main__':
-    unittest.main()
